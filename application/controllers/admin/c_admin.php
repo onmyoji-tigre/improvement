e7141278b613547151e0ca59b5eb1b0dc75ceac8
onmyoji-tigre <nexglobaltop@gmail.com>
2019-01-18 08:52:13 +0700

    penambahan pada admin/c_admin.php

--- application/controllers/admin/c_admin.php
+++ application/controllers/admin/c_admin.php
@@ -4,18 +4,19 @@ class C_admin extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		if ($this->session->userdata('username')=="") {
+		if ($this->session->userdata('nik_karyawan')=="") {
 			redirect('auth');
 		}
 		$this->load->helper('text');
 	}
 	public function index() {
-		$data['username'] = $this->session->userdata('username');
+		$data['nik_karyawan'] = $this->session->userdata('nik_karyawan');
+		$data['nama_karyawan'] = $this->session->userdata('nama_karyawan');
 		$this->load->view('admin/index', $data);
 	}
 
 	public function logout() {
-		$this->session->unset_userdata('username');
+		$this->session->unset_userdata('nik_karyawan');
 		$this->session->unset_userdata('level');
 		$this->session->session_destroy();
 		redirect('auth');
